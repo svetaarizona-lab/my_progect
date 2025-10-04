667c0e928f4023587e1cc23a697f493a72307a7f
Admin <svetaarizona@gmail.com>
2025-10-04 21:30:04 +0300

    homework

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print('hello world')
+print("hello world")
+
+number = int(input("Please enter the integer number 4-x: "))
+digit1 = number // 1000
+digit2 = (number // 100) % 10
+digit3 = (number // 10) % 10
+digit4 = number % 10
+print(digit1)
+print(digit2)
+print(digit3)
+print(digit4)
+
+
+
