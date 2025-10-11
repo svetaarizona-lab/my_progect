0e32c721b8b90686c7adbd60a5928dcecef4424d
Admin <svetaarizona@gmail.com>
2025-10-11 11:07:48 +0300

    hom3.2

---
+++ homew3.2.py
@@ -0,0 +1,5 @@
+lst1 = [12, 3, 4, 10, 12, 3, 4]
+if lst1:
+    last = lst1.pop()
+    lst1.insert (0,last)
+    print(lst1 )
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 print('hello world')
 
-number = int(input("Please enter the integer number 4-x: "))
+number = int(input("Please:  enter the integer number 4-x"))
 digit1 = number // 1000
 digit2 = (number // 100) % 10
 digit3 = (number // 10) % 10
