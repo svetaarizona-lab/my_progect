a6cce69a0081e69b9c98f4be85454602455aac94
Admin <svetaarizona@gmail.com>
2025-10-12 10:42:15 +0300

    hom3.3

--- home3.3.py
+++ home3.3.py
@@ -1,17 +1,12 @@
-example = [1] , [], [[1, 2, 3], [4, 5, 6]] # можна змінювати для перевірки
-
-if not example:
-    pr = [[], []]
-    print("empty list", pr)
+example = [1, 2, 3, 4, 5, 6]
+if len(example) == 0: #порожній
+    result = [[], []]
+    print("Порожній:", result)
 else:
-    middle = len(example) // 2 #middle of the list
-    if len(example) % 2 == 0: #even numbers
-        first = example[:middle]
-        second = example[middle:]
-        pr = [first, second]
-        print("even numbers", pr)
-    else: #odd numbers
-        first = example[:middle]
-        second = example[middle:]
-        pr = [first, second]
-        print("odd numbers", pr)
+    middle = len(example) // 2 #середина
+    if len(example) % 2 != 0: #непарна
+        middle += 1
+    first = example[:middle]
+    second = example[middle:]
+    result = [first, second]
+    print("Результат:", result)
